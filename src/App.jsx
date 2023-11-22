6a4ac5fd935bcc53dbd3e95bd94641f393ad3b41
Pusaka Manggala <pusakamanggala@gmail.com>
2023-11-22 20:53:48 +0700

    feat : routing and layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import SalLayout from "./layout/SalLayout";
+import JobVacancy from "./pages/JobVacancy";
+import LandingPage from "./pages/LandingPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <SalLayout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/vacancy" element={<JobVacancy />} />
+        </Routes>{" "}
+      </SalLayout>
+    </BrowserRouter>
+  );
 }
