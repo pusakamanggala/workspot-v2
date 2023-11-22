c4c5912af1e9b2d5cf622a7ac5144886a13d0b8f
Pusaka Manggala <pusakamanggala@gmail.com>
2023-11-22 20:51:55 +0700

    Add SalLayout component with sal.js initialization

---
+++ src/layout/SalLayout.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import sal from "sal.js";
+import "sal.js/dist/sal.css";
+import { useLocation } from "react-router-dom";
+import PropTypes from "prop-types";
+
+// this is a layout that will initialize sal.js on every page that use this layout
+const SalLayout = ({ children }) => {
+  const location = useLocation();
+  // initialize Sal on every route change
+  useEffect(() => {
+    sal();
+  }, [location]);
+
+  return <div>{children}</div>;
+};
+
+SalLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default SalLayout;
