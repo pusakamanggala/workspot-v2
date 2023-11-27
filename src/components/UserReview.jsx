c90667aa93849c47433f247ed9184b164341dbdb
Pusaka Manggala <pusakamanggala@gmail.com>
2023-11-27 16:26:15 +0700

    change image to static

--- src/components/UserReview.jsx
+++ src/components/UserReview.jsx
@@ -1,87 +1,99 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import JohnDoePict from "../img/reviewer/john_doe.webp";
+import JaneSmithPict from "../img/reviewer/jane_smith.webp";
+import MikeJohnsonPict from "../img/reviewer/mike_johnson.webp";
+import SarahBrownPict from "../img/reviewer/sarah_brown.webp";
+import AlexWilsonPict from "../img/reviewer/alex_wilson.webp";
+import EmilyDavisPict from "../img/reviewer/emily_davis.webp";
+import MichaelLeePict from "../img/reviewer/michael_lee.webp";
+import SophiaClarkPict from "../img/reviewer/sophia_clark.webp";
+import DanielWhitePict from "../img/reviewer/daniel_white.webp";
+import OliviaAdamsPict from "../img/reviewer/olivia_adams.webp";
+import WilliamBrownPict from "../img/reviewer/william_brown.webp";
+import EllaHarrisPict from "../img/reviewer/ella_harris.webp";
 
 const UserReviews = () => {
   const reviewsData = [
     {
-      profileImg: `https://xsgames.co/randomusers/avatar.php?g=male&seed=${Math.random()}`,
+      profileImg: JohnDoePict,
       name: "John Doe",
       job: "IT Professional",
       review:
         "I had a great experience using the platform. It helped me find the perfect job!",
     },
     {
-      profileImg: `https://xsgames.co/randomusers/avatar.php?g=female&seed=${Math.random()}`,
+      profileImg: JaneSmithPict,
       name: "Jane Smith",
       job: "Software Engineer",
       review:
         "The platform made my job search easier and more efficient. Highly recommended!",
     },
     {
-      profileImg: `https://xsgames.co/randomusers/avatar.php?g=male&seed=${Math.random()}`,
+      profileImg: MikeJohnsonPict,
       name: "Mike Johnson",
       job: "Web Developer",
       review: "I found my dream job through this website. It's a game-changer!",
     },
     {
-      profileImg: `https://xsgames.co/randomusers/avatar.php?g=female&seed=${Math.random()}`,
+      profileImg: SarahBrownPict,
       name: "Sarah Brown",
       job: "Graphic Designer",
       review:
         "This platform is fantastic! It connected me with great job opportunities in my field.",
     },
     {
-      profileImg: `https://xsgames.co/randomusers/avatar.php?g=male&seed=${Math.random()}`,
+      profileImg: AlexWilsonPict,
       name: "Alex Wilson",
       job: "Data Scientist",
       review:
         "As a data scientist, I appreciate the data-driven job recommendations. It's impressive!",
     },
     {
-      profileImg: `https://xsgames.co/randomusers/avatar.php?g=female&seed=${Math.random()}`,
+      profileImg: EmilyDavisPict,
       name: "Emily Davis",
       job: "Marketing Specialist",
       review:
         "I love the user-friendly interface. It made my job search hassle-free and efficient.",
     },
     {
-      profileImg: `https://xsgames.co/randomusers/avatar.php?g=male&seed=${Math.random()}`,
+      profileImg: MichaelLeePict,
       name: "Michael Lee",
       job: "UX/UI Designer",
       review:
         "The platform's design and usability are top-notch. It's a valuable resource for job seekers.",
     },
     {
-      profileImg: `https://xsgames.co/randomusers/avatar.php?g=female&seed=${Math.random()}`,
+      profileImg: SophiaClarkPict,
       name: "Sophia Clark",
       job: "Project Manager",
       review:
         "I found my dream project management job through this platform. It's a career-changer!",
     },
     {
-      profileImg: `https://xsgames.co/randomusers/avatar.php?g=male&seed=${Math.random()}`,
+      profileImg: DanielWhitePict,
       name: "Daniel White",
       job: "Sales Executive",
       review:
         "The platform's job matching algorithms are excellent. It connected me with my ideal job.",
     },
     {
-      profileImg: `https://xsgames.co/randomusers/avatar.php?g=female&seed=${Math.random()}`,
+      profileImg: OliviaAdamsPict,
       name: "Olivia Adams",
       job: "Software Developer",
       review:
         "I was able to land a great software development job using this platform. It's a game-changer!",
     },
     {
-      profileImg: `https://xsgames.co/randomusers/avatar.php?g=male&seed=${Math.random()}`,
+      profileImg: WilliamBrownPict,
       name: "William Brown",
       job: "Network Engineer",
       review:
         "As a network engineer, I found the perfect job opportunity through this platform. Highly recommended!",
     },
     {
-      profileImg: `https://xsgames.co/randomusers/avatar.php?g=female&seed=${Math.random()}`,
+      profileImg: EllaHarrisPict,
       name: "Ella Harris",
       job: "Marketing Manager",
       review:
