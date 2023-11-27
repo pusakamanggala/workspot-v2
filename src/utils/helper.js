1dc2a407f450bd4b669e79b013558b2bd2cd5f3a
Pusaka Manggala <pusakamanggala@gmail.com>
2023-11-27 16:40:28 +0700

    fix : convert date function

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,27 +1,24 @@
-export const convertDate = (timestamp) => {
-  const currentDate = new Date();
-  const createdAt = new Date(timestamp);
-  const timeDifference = currentDate - createdAt;
-
+export const convertDate = (created_at) => {
+  const now = new Date();
+  const createdAtDate = new Date(created_at);
+  const timeDifference = now - createdAtDate;
   const seconds = Math.floor(timeDifference / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
   const days = Math.floor(hours / 24);
-  const weeks = Math.floor(days / 7);
   const months = Math.floor(days / 30);
+  const years = Math.floor(months / 12);
 
-  if (seconds < 60) {
-    return `${seconds}s ago`;
-  } else if (minutes < 60) {
-    return `${minutes}min ago`;
-  } else if (hours < 24) {
-    return `${hours}h ago`;
-  } else if (days < 7) {
-    return `${days}d ago`;
-  } else if (weeks < 4) {
-    return `${weeks}w ago`;
+  if (days === 0) {
+    return "New";
+  } else if (days === 1) {
+    return "1 days ago";
+  } else if (days <= 30) {
+    return `${days} days ago`;
+  } else if (days <= 365) {
+    return `${months} month ago`;
   } else {
-    return `${months}m ago`;
+    return `${years} years ago`;
   }
 };
 
